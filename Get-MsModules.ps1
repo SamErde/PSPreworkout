39948a23f68fb72c4238dd820e773a15ee7ff972
Sam Erde <20478745+SamErde@users.noreply.github.com>
2023-11-30 15:12:17 -0500

    Initial working POC

---
+++ Get-MsModules.ps1
@@ -0,0 +1,31 @@
+<#
+    .DESCRIPTION
+        A quick proof of concept for pulling a list of Microsoft PowerShell modules from the msshells.net project.
+
+    .NOTES
+        Credit and thanks to Andrés Gorzelany for permission to depend on the MSSHELLS.NET project for its data!
+
+        See https://github.com/get-itips/msshells, https://twitter.com/AndresGorzelany
+#>
+
+function Get-MsShellsModules {
+    $MsShellsRepoUri = 'https://api.github.com/repos/get-itips/msshells/contents/_ps_modules'
+    $Content = Invoke-RestMethod -Uri $MsShellsRepoUri
+
+    foreach ($item in $Content) {
+        $DownloadPath = "msshells-content/$($item.name)"
+        Invoke-RestMethod -Uri $item.download_url -OutFile $DownloadPath
+    }
+}
+
+function Read-ModuleInfo {
+    Install-Module powershell-yaml
+    Import-Module powershell-yaml
+
+    $PsModuleFiles = Get-ChildItem -Path 'msshells-content/*.md'
+    foreach ($item in $PsModuleFiles) {
+
+        $Module = ConvertFrom-Yaml -Yaml ([System.IO.File]::ReadAllText("$item"))
+        Find-Module -Name $Module.name
+    }
+}
