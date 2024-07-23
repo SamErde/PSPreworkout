e5e38ef3b09111ae83ef0d232574995ce10c4aba
Sam Erde <samuel.erde@gmail.com>
2024-07-23 15:50:31 -0400

    Bumped the version

--- Update-AllTheThings.ps1
+++ Update-AllTheThings.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 .DESCRIPTION A script to automatically update all PowerShell modules, PowerShell Help, and packages (apt, brew, chocolately, winget).
-.VERSION 0.3.0-preview
+.VERSION 0.3.1-preview
 .GUID 3a1a1ec9-0ef6-4f84-963d-be1505dab6a8
 .AUTHOR Sam Erde
 .COPYRIGHT (c) Sam Erde
@@ -35,7 +35,7 @@ function Update-AllTheThings {
 
     .NOTES
     Author: Sam Erde
-    Version: 0.3.0-preview
+    Version: 0.3.1-preview
     Modified: 2024/07/23
 
     Ideas to do:
