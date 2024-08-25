90a239816fc03a1fd633a598af961188a6afc74f
Sam Erde <20478745+SamErde@users.noreply.github.com>
2024-08-25 15:36:58 -0400

    Add New-Credential.ps1

---
+++ src/PSPreworkout/Public/New-Credential.ps1
@@ -0,0 +1,17 @@
+function New-Credential {
+    <#
+    .SYNOPSIS
+    Create a new secure credential.
+    
+    .DESCRIPTION
+    Create a new secure credential to use in other functions.
+    
+    .EXAMPLE
+    $Credential = New-Credential
+    #>
+    Write-Output "Create a Credential"
+    $User = Read-Host -Prompt "User"
+    $Password = Read-Host "Password for user $User" -AsSecureString
+    $Credential = [System.Management.Automation.PSCredential]::New($User, $Password)
+    $Credential
+} # end function New-Credential
