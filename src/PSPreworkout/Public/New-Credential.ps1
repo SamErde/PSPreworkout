0a2c194c4439932d56f79177e9c2ce2a2a064cc4
Sam Erde <samuel.erde@gmail.com>
2024-08-27 05:29:59 -0400

    Add empty param block to make checks happy

--- src/PSPreworkout/Public/New-Credential.ps1
+++ src/PSPreworkout/Public/New-Credential.ps1
@@ -2,17 +2,18 @@ function New-Credential {
     <#
     .SYNOPSIS
     Create a new secure credential.
-    
+
     .DESCRIPTION
     Create a new secure credential to use in other functions.
-    
+
     .EXAMPLE
     $Credential = New-Credential
     #>
     [CmdletBinding()]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Justification = 'OK')]
-    Write-Output "Create a Credential"
-    $User = Read-Host -Prompt "User"
+    param ()
+    Write-Output 'Create a Credential'
+    $User = Read-Host -Prompt 'User'
     $Password = Read-Host "Password for user $User" -AsSecureString
     $Credential = [System.Management.Automation.PSCredential]::New($User, $Password)
     $Credential
