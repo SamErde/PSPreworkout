f8a86ccc75239e8e8a1c9f038f000ab763b39618
Sam Erde <20478745+SamErde@users.noreply.github.com>
2024-08-30 09:07:11 -0400

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -2,24 +2,7 @@ param  (
   [string] $PSGalleryApiKey
 )
 
-# Purpose: Cleans the module directory to prepare for publication to PowerShell Gallery
 $ErrorActionPreference = 'stop'
 $ModulePath = "./src/PSPreworkout"
 
-# Build directory should work on Windows or Linux
-$BuildDirectory = $env:TEMP ? "$env:TEMP/Build" : "/tmp/Build"
-
-# These project paths will be excluded from the module during publishing
-$Exclude = @(
-)
-
-# Copy project to temporary build directory
-if (Test-Path -Path $BuildDirectory) {
-  Remove-Item -Path $BuildDirectory -Recurse -Force
-}
-$null = New-Item -ItemType Directory -Path $BuildDirectory
-Write-Host -Object 'Created build directory'
-Copy-Item -Exclude $Exclude -Path $ModulePath/* -Destination $BuildDirectory -Recurse
-Write-Host -Object 'Copied all items to build directory'
-
-Publish-Module -Path $BuildDirectory -NuGetApiKey $PSGalleryApiKey
+Publish-Module -Path $ModulePath -NuGetApiKey $PSGalleryApiKey
