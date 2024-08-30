8cecf25664102e9fb2a44a2eadd6f3c5b3cbcbab
Sam Erde <samuel.erde@gmail.com>
2024-08-30 06:14:19 -0400

    Add Test-IsElevated function.

---
+++ src/PSPreworkout/Public/Test-IsElevated.ps1
@@ -0,0 +1,26 @@
+function Test-IsElevated {
+    <#
+    .SYNOPSIS
+    Check if you are running an elevated shell with administrator or root privileges.
+
+    .DESCRIPTION
+    Check if you are running an elevated shell with administrator or root privileges.
+
+    .EXAMPLE
+    Test-IsElevated
+
+    .OUTPUTS
+    Boolean
+    #>
+    [CmdletBinding()]
+    [Alias('isadmin', 'isroot')]
+    param ()
+
+    if (($PSVersionTable.PSVersion.Major -le 5) -or $IsWindows) {
+        $CurrentUser = [Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())
+        return $CurrentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    } else {
+        # Must be Linux or OSX, so use the id util. Root has userid of 0.
+        return 0 -eq (id -u)
+    }
+}
