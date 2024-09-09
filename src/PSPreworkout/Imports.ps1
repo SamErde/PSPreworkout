abd3d1bb72f082ca9a69d95fc5c2b30806d68f64
Sam Erde <samuel.erde@gmail.com>
2024-09-09 09:27:15 -0400

    Cleanup scaffolding.

--- src/PSPreworkout/Imports.ps1
+++ src/PSPreworkout/Imports.ps1
@@ -2,5 +2,5 @@
 # It can be imported by the psm1 in local development to add script level variables.
 # It will merged in the build process. This is for local development only.
 
-# region script variables
+# region Script Variables
 # $script:resourcePath = "$PSScriptRoot\Resources"
