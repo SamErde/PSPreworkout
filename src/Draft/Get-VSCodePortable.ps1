942d4cd581d15101f736ce2a0ad010f356877dfc
Sam Erde <samuel.erde@gmail.com>
2024-09-11 17:19:10 -0400

    Early draft of new scripts

---
+++ src/Draft/Get-VSCodePortable.ps1
@@ -0,0 +1,42 @@
+function Get-VSCodePortable {
+    <#
+        .SYNOPSIS
+        Download a portable version of Visual Studio Code to run anywhere on demand.
+
+        .DESCRIPTION
+        This function helps you download a zipped version of Visual Studio Code that can be run anywhere without needing to install it.
+
+        .PARAMETER Parameter1
+        __PARAMETER1__
+
+        .PARAMETER Parameter2
+        __PARAMETER2__
+
+        .EXAMPLE
+        __EXAMPLE__
+
+        .NOTES
+        Author: Sam Erde
+        Version: 0.0.1
+        Modified: 2024-09-11
+    #>
+
+    [CmdletBinding()]
+    [Alias('Get-Code')]
+    param (
+
+    )
+
+    begin {
+
+    } # end begin block
+
+    process {
+
+    } # end process block
+
+    end {
+
+    } # end end block
+
+} # end function Get-VSCodePortable
