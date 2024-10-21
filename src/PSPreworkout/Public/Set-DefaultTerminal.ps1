be2f8f918a1a0999c718f0fd90fad18a6fe85d91
Sam Erde <samuel.erde@gmail.com>
2024-10-21 17:10:02 -0400

    Stage new function.

---
+++ src/PSPreworkout/Public/Set-DefaultTerminal.ps1
@@ -0,0 +1,36 @@
+function Set-DefaultTerminal {
+    <#
+        .SYNOPSIS
+        Set the default terminal in Windows.
+
+        .DESCRIPTION
+        This function sets that default terminal in Windows to your choice of Windows PowerShell, PowerShell, Command Prompt, or Windows Terminal (default).
+
+        .EXAMPLE
+        Set-DefaultTerminal
+
+        .NOTES
+        Author: Sam Erde
+        Version: 0.0.1
+        Modified: 2024-10-21
+    #>
+
+    [CmdletBinding()]
+
+    param (
+
+    )
+
+    begin {
+
+    } # end begin block
+
+    process {
+
+    } # end process block
+
+    end {
+
+    } # end end block
+
+} # end function Set-DefaultTerminal
