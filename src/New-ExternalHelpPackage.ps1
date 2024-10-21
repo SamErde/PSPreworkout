bd18dbd23f3387fe59f7c753b9bbf2dc17a733b2
Sam Erde <samuel.erde@gmail.com>
2024-10-20 23:18:45 -0400

    External Help

--- src/New-ExternalHelpPackage.ps1
+++ src/New-ExternalHelpPackage.ps1
@@ -1,6 +1,7 @@
-﻿$params = @{
-    CabFilesFolder  = "$PSScriptRoot\Artifacts\en-US"
+﻿Copy-Item "$PSScriptRoot\Artifacts\en-US\PSPreworkout-help.xml" "$PSScriptRoot\Help\en-US"
+$params = @{
+    CabFilesFolder  = "$PSScriptRoot\Help\en-US"
     LandingPagePath = "$PSScriptRoot\..\docs\PSPreworkout.md"
-    OutputFolder    = "$PSScriptRoot\Artifacts\en-US"
+    OutputFolder    = "$PSScriptRoot\Help\en-US"
 }
 New-ExternalHelpCab @params
