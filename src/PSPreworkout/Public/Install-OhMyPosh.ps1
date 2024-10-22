6117fe6315a01056b5e750d95300e578c7bf421a
Sam Erde <samuel.erde@gmail.com>
2024-10-22 10:59:46 -0400

    Fix a whoops

--- src/PSPreworkout/Public/Install-OhMyPosh.ps1
+++ src/PSPreworkout/Public/Install-OhMyPosh.ps1
@@ -32,7 +32,7 @@ function Install-OhMyPosh {
     .EXAMPLE
         Install-OhMyPosh
 #>
-    [CmdletBinding(HelpUri = 'https://raw.githubusercontent.com/SamErde/PSPreworkout/main/src/Help/')]lpUri = 'https://raw.githubusercontent.com/SamErde/PSPreworkout/main/src/Help/')]
+    [CmdletBinding(HelpUri = 'https://raw.githubusercontent.com/SamErde/PSPreworkout/main/src/Help/')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'Invoke-Expression is used for online OMP installer.')]
     param (
         [Parameter()]
