4f99bf71363ce2159eb9616c20470299b53ccc7f
Sam Erde <samuel.erde@gmail.com>
2024-11-04 10:42:23 -0500

    Refactor and add tests

---
+++ src/Tests/Test-GEV.ps1
@@ -0,0 +1,66 @@
+﻿
+function Test-GEV {
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWriteHost', '', Justification = 'Interactive Use')]
+    param ()
+
+    Clear-Host
+    # Expected: Return all environment variables from all targets.
+    # Results:  Works as expected.
+    Write-Host 'Nothing' -ForegroundColor Magenta -BackgroundColor Black
+    Get-EnvironmentVariable -Debug | Format-Table Name, Target, Value
+
+    Clear-Host
+    # Expected: Return all environment variables from all targets.
+    # Results:  Works as expected.
+    Write-Host 'Everything' -ForegroundColor Magenta -BackgroundColor Black
+    Get-EnvironmentVariable -All -Debug | Format-Table Name, Target, Value
+
+    Clear-Host
+    # Expected: Return all name matches in the specified targets.
+    # Results:  Works as expected.
+    Write-Host '-Name with Targets' -ForegroundColor Magenta -BackgroundColor Black
+    Get-EnvironmentVariable -Name TEMP -Target 'Machine' -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Name TEMP -Target 'Machine', 'User' -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Name TEMP -Target 'Machine', 'User', 'Process' -Debug | Format-Table Name, Target, Value
+
+    Clear-Host
+    # Expected: Return all pattern matches in the specified targets.
+    # Results:  Works as expected.
+    Write-Host '-Pattern with Targets' -ForegroundColor Magenta -BackgroundColor Black
+    Get-EnvironmentVariable -Pattern '^t' -Target 'Machine' -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Pattern '^t' -Target 'Machine', 'User' -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Pattern '^t' -Target 'Machine', 'User', 'Process' -Debug | Format-Table Name, Target, Value
+
+    Clear-Host
+    # Expected: Return all environment variables from the specified target[s].
+    # Results:  Works as expected.
+    Write-Host 'Just Targets' -ForegroundColor Cyan -BackgroundColor Black
+    Get-EnvironmentVariable -Target 'Machine' -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Target 'Machine', 'User' -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Target 'Machine', 'User', 'Process' -Debug | Format-Table Name, Target, Value
+
+    Clear-Host
+    # Expected: Return all name matches in the specified target[s]. Ignore the -All switch.
+    # Results:  Works as expected.
+    Write-Host '-Name with Targets & All' -ForegroundColor Yellow -BackgroundColor Black
+    Get-EnvironmentVariable -Name TEMP -Target 'Machine' -All -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Name TEMP -Target 'Machine', 'User' -All -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Name TEMP -Target 'Machine', 'User', 'Process' -All -Debug | Format-Table Name, Target, Value
+
+    Clear-Host
+    # Expected: Return all pattern matches in the specified target[s]. Ignore the -All switch.
+    # Results:  Works as expected.
+    Write-Host '-Pattern with Targets & All' -ForegroundColor Green -BackgroundColor Black
+    Get-EnvironmentVariable -Pattern '^t' -Target 'Machine' -All -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Pattern '^t' -Target 'Machine', 'User' -All -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Pattern '^t' -Target 'Machine', 'User', 'Process' -All -Debug | Format-Table Name, Target, Value
+
+    Clear-Host
+    # Expected: Return all environment variables from the specified target[s].
+    # Results:  Works as expected.
+    Write-Host 'Just Targets & All' -ForegroundColor Cyan -BackgroundColor Black
+    Get-EnvironmentVariable -Target 'Machine' -All -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Target 'Machine', 'User' -All -Debug | Format-Table Name, Target, Value
+    Get-EnvironmentVariable -Target 'Machine', 'User', 'Process' -All -Debug | Format-Table Name, Target, Value
+}
