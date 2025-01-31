a52e92eee9c6ccfa35f2fda3696448c6b123ec45
Sam Erde <samuel.erde@gmail.com>
2025-01-31 13:04:20 -0500

    Re-enable integration test.

--- src/Tests/Integration/SampleIntegrationTest.Tests.ps1
+++ src/Tests/Integration/SampleIntegrationTest.Tests.ps1
@@ -7,7 +7,6 @@ BeforeAll {
     Import-Module $PathToManifest -Force
 }
 
-<# RE-ENABLE AFTER REFACTORING 'NEW-SCRIPTFROMTEMPLATE'
 Describe 'Integration Tests' -Tag Integration {
     Context 'First Integration Tests' {
         It 'should pass the first integration test' {
@@ -17,4 +16,3 @@ Describe 'Integration Tests' -Tag Integration {
 }
 
 Remove-Module $ModuleName -Force
-#>
