0804efc7cb14a1bd5071679e09626f65ce485d64
Sam Erde <samuel.erde@gmail.com>
2025-02-25 11:17:57 -0500

    Final fix external help process. Move and cleanup.

--- src/New-ExternalHelpPackage.ps1
+++ src/New-ExternalHelpPackage.ps1
@@ -1,7 +1,6 @@
-﻿Copy-Item "$PSScriptRoot\Artifacts\en-US\PSPreworkout-help.xml" "$PSScriptRoot\PSPreworkout\en-US"
-$params = @{
+﻿$params = @{
     CabFilesFolder  = "$PSScriptRoot\Artifacts\en-US"
     LandingPagePath = "$PSScriptRoot\..\docs\PSPreworkout.md"
-    OutputFolder    = "$PSScriptRoot\..docs\en-US"
+    OutputFolder    = "$PSScriptRoot\..\docs\en-US\"
 }
 New-ExternalHelpCab @params -IncrementHelpVersion
