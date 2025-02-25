294af1f9a4c35081f2d76cbf18d4382fe5a1073c
Sam Erde <samuel.erde@gmail.com>
2025-02-25 10:45:14 -0500

    Fix CabFilesFolder value. Move output folder. Auto-increment help version.

--- src/New-ExternalHelpPackage.ps1
+++ src/New-ExternalHelpPackage.ps1
@@ -1,7 +1,7 @@
-﻿Copy-Item "$PSScriptRoot\Artifacts\en-US\PSPreworkout-help.xml" "$PSScriptRoot\..\Help\en-US"
+﻿Copy-Item "$PSScriptRoot\Artifacts\en-US\PSPreworkout-help.xml" "$PSScriptRoot\PSPreworkout\en-US"
 $params = @{
-    CabFilesFolder  = "$PSScriptRoot\..\Help\en-US"
+    CabFilesFolder  = "$PSScriptRoot\Artifacts\en-US"
     LandingPagePath = "$PSScriptRoot\..\docs\PSPreworkout.md"
-    OutputFolder    = "$PSScriptRoot\..\Help\en-US"
+    OutputFolder    = "$PSScriptRoot\PSPreworkout\en-US"
 }
-New-ExternalHelpCab @params
+New-ExternalHelpCab @params -IncrementHelpVersion
