54b5f06677aac5772b128145e5d8a343acb01d2c
Sam Erde <samuel.erde@gmail.com>
2025-06-28 15:46:23 -0400

    Remove unnecessary elements

--- src/PSPreworkout/Public/Edit-PSReadLineHistoryFile.ps1
+++ src/PSPreworkout/Public/Edit-PSReadLineHistoryFile.ps1
@@ -12,62 +12,21 @@ function Edit-PSReadLineHistoryFile {
 
     .NOTES
     Author: Sam Erde
-    Version: 0.0.2
+    Version: 0.0.3
     Modified: 2025-01-06
-
-    Idea: History search with a TUI? This may already be done by the PowerShell Run module.
     #>
 
     [CmdletBinding(HelpUri = 'https://day3bits.com/PSPreworkout/Edit-PSReadLineHistoryFile')]
     [Alias('Edit-HistoryFile')]
-    param (
+    param ( )
 
-    )
-
-    begin {
-        $HistoryFilePath = (Get-PSReadLineOption).HistorySavePath
-    } # end begin block
-
-    process {
-        if ((Get-Command code)) {
-            # Open the file in Visual Studio Code if code found
-            code $HistoryFilePath
-
-            <#
-            if ((Get-Command node -ErrorAction SilentlyContinue)) {
-                # Use the Visual Studio Code API to set the open file's language (this portion rendered by AI).
-                $VSCodeScript = @'
-const vscode = require('vscode');
-
-function setLanguage(uri, language) {
-    vscode.workspace.openTextDocument(uri).then(document => {
-        vscode.window.showTextDocument(document).then(() => {
-            vscode.languages.setTextDocumentLanguage(document, language);
-        });
-    });
-}
-
-const uri = vscode.Uri.file(process.argv[2]);
-setLanguage(uri, 'powershell');
-'@ # End VSCodeScript (this portion rendered by AI)
-
-                # Save the VS Code script to a temporary file and then run it.
-                $TempScriptFile = [System.IO.Path]::GetTempFileName() + '.js'
-                Set-Content -Path $TempScriptFile -Value $VSCodeScript -Force
-                node $TempScriptFile "`'$HistoryFilePath`'"
-            }
-#> # End scriptblock to set the file language to 'PowerShell'
-
-        } else {
-            # Open the text file with the default file handler if VS Code is not found.
-            Start-Process $HistoryFilePath
-        }
-    } # end process block
-
-    end {
-        if ($TempScriptFile) {
-            #Remove-Item -Path $TempScriptFile -Confirm:$false -ErrorAction SilentlyContinue | Out-Null
-        }
-    } # end end block
+    $HistoryFilePath = (Get-PSReadLineOption).HistorySavePath
+    if ((Get-Command code)) {
+        # Open the file in Visual Studio Code if code found
+        code $HistoryFilePath
+    } else {
+        # Open the text file with the default file handler if VS Code is not found.
+        Start-Process $HistoryFilePath
+    }
 
 } # end function Edit-PSreadLineHistoryFile
