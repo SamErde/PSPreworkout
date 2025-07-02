874fde20b84e2020a2e4dc8b37ebce0fcd97005c
Sam Erde <samuel.erde@gmail.com>
2025-07-02 16:35:35 -0400

    Enhance documentation and error handling in Set-DefaultTerminal function

--- src/PSPreworkout/Public/Set-DefaultTerminal.ps1
+++ src/PSPreworkout/Public/Set-DefaultTerminal.ps1
@@ -1,21 +1,17 @@
 function Set-DefaultTerminal {
     <#
     .SYNOPSIS
-    Configure the default terminal for Windows
+    Configure the default terminal for Windows.
 
     .DESCRIPTION
-    This function sets that default terminal in Windows to your choice of Windows PowerShell, PowerShell, Command Prompt, or Windows Terminal (default).
+    This function sets that default terminal in Windows to your choice of Windows PowerShell, PowerShell, Command Prompt,
+    or Windows Terminal (default).
 
     .PARAMETER Name
     The name of the application to use as the default terminal in Windows.
 
     .EXAMPLE
     Set-DefaultTerminal -Name 'WindowsTerminal'
-
-    .NOTES
-    Author: Sam Erde
-    Version: 0.0.1
-    Modified: 2024-11-15
     #>
 
     [CmdletBinding(SupportsShouldProcess)]
@@ -29,7 +25,11 @@ function Set-DefaultTerminal {
     begin {
         $KeyPath = 'HKCU:\Console\%%Startup'
         if (-not (Test-Path -Path $keyPath)) {
-            New-Item -Path $KeyPath | Out-Null
+            try {
+                New-Item -Path $KeyPath >$null
+            } catch {
+                throw "Failed to create registry key: $_"
+            }
         } else {
             Write-Verbose -Message "Key already exists: $KeyPath"
         }
@@ -38,8 +38,12 @@ function Set-DefaultTerminal {
     process {
         switch ($Name) {
             'WindowsTerminal' {
-                New-ItemProperty -Path 'HKCU:\Console\%%Startup' -Name 'DelegationConsole' -Value '{2EACA947-7F5F-4CFA-BA87-8F7FBEEFBE69}' -Force | Out-Null
-                New-ItemProperty -Path 'HKCU:\Console\%%Startup' -Name 'DelegationTerminal' -Value '{E12CFF52-A866-4C77-9A90-F570A7AA2C6B}' -Force | Out-Null
+                try {
+                    New-ItemProperty -Path 'HKCU:\Console\%%Startup' -Name 'DelegationConsole' -Value '{2EACA947-7F5F-4CFA-BA87-8F7FBEEFBE69}' -Force >$null
+                    New-ItemProperty -Path 'HKCU:\Console\%%Startup' -Name 'DelegationTerminal' -Value '{E12CFF52-A866-4C77-9A90-F570A7AA2C6B}' -Force >$null
+                } catch {
+                    throw "Failed to set default terminal: $_"
+                }
             }
             default {
                 Write-Information -MessageData 'No terminal application was specified.' -InformationAction Continue
