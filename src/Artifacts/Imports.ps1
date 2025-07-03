0a737346e45fbd0dc7c48f177f7dff417e6cea53
Sam Erde <samuel.erde@gmail.com>
2025-07-03 11:15:37 -0400

    Test Build

--- src/Artifacts/Imports.ps1
+++
@@ -1,7 +0,0 @@
-# This is a locally sourced Imports file for local development.
-# It can be imported by the psm1 in local development to add script level variables.
-# It will merged in the build process. This is for local development only.
-
-# region Script Variables
-# $script:resourcePath = "$PSScriptRoot\Resources"
-Export-ModuleMember -Alias *
