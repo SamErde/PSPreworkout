0e5d6a138fdde5fa3e931db784b910e604b0750a
Sam Erde <samuel.erde@gmail.com>
2025-07-31 12:51:37 -0400

    Cleanup documentation

--- src/New-ExternalHelpPackage.ps1
+++
@@ -1,6 +0,0 @@
-﻿$params = @{
-    CabFilesFolder  = "$PSScriptRoot\Artifacts\en-US"
-    LandingPagePath = "$PSScriptRoot\..\docs\PSPreworkout.md"
-    OutputFolder    = "$PSScriptRoot\..\docs\en-US\"
-}
-New-ExternalHelpCab @params
